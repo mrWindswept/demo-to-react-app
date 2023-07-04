72c99e4a2b0dafbcd0d2a34a5fc464e42997404b
Kunal Patil <kunallanuk9@gmail.com>
2023-07-04 17:43:53 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 /**
  * insert a value at particular index
- * e.g. insert 5 at 2nd index or after 1st index
+ * e.g. insert 5 at 2nd index
  * @param {startIndex} number
  * @param {deleteCount} number
  * @param {valueToInsert} number
@@ -8,11 +8,13 @@
 
 const x = [1, 2, 3, 4, 10];
 x.splice(2,0,5)
+// new value for x - [1, 2, 5, 3, 4, 10]
 
 
 /**
- * add / copy existing array into another array
+ * add or copy existing array into another array
  */
 
 const y = [5,6,7,8,9]
 const z = [...y,10,11,12]
+// new value for z - [5, 6, 7, 8, 9, 10, 11, 12]
